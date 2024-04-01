dfda1993c5853875327443f585c355c8422c44c4
thammo4 <thammo4@uic.edu>
2024-04-01 09:31:00 -0400

    Add a convenience javascript command to retrieve the commons identifier from the browser

---
+++ commons_id_console_command.js
@@ -0,0 +1,8 @@
+//
+// When viewing a wikimedia commons file, run this from the browser's javascript console to readily obtain the commons identifier
+// This is useful when spot checking / investigating a single record
+// Ex: https://commons.wikimedia.org/wiki/File:Nirvana_around_1992.jpg)
+// 		mw.config.get('wgArticleId')
+// 		1314918
+
+mw.config.get('wgArticleId');
