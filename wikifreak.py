64b27d24c8c9e664cf0b57ff33c525597c9b866f
thammo4 <thammo4@uic.edu>
2024-04-02 22:13:10 -0400

    Update dataframe construction functionality to accommodate commons uploads with multiple depicts statements

--- wikifreak.py
+++ wikifreak.py
@@ -28,10 +28,10 @@ def get_random_commons_ids (num_files=1):
 
 
 #
-# Function to map WikiMedia Commons uploads to their corresponding WikiData Entities
+# Function to map WikiMedia Commons uploads to their corresponding WikiData Entities (Q#s)
 #
 
-def get_q_number (wc_page_id):
+def get_q_numbers (wc_page_id, verbose=False):
     # Construct the WikiBase Entity ID
     wb_entity_id = f'M{wc_page_id}'
 
@@ -51,29 +51,29 @@ def get_q_number (wc_page_id):
     # Check if the entity exists and has statements
     if 'entities' not in data or wb_entity_id not in data['entities'] or 'statements' not in data['entities'][wb_entity_id]:
         print(f"No data available for Wikimedia Commons Page ID: {wc_page_id}")
-        return None
+        return [];
 
     data_statements = data['entities'][wb_entity_id]['statements']
 
     # Check if 'P180' (depicts) property exists
     if 'P180' not in data_statements or not data_statements['P180']:
         print(f"No 'depicts' statements for Wikimedia Commons Page ID: {wc_page_id}")
-        return 0;
+        return [];
 
-    # Assuming 'P180' exists and has at least one entry
-    depicts = data_statements['P180'][0]
+    # Isolate depicts with the `depicts` property value
+    depicts = data_statements['P180'];
 
-    # Extract relevant information pertaining to the `Depicts` statement
-    depicts_data = depicts['mainsnak']
+    q_numbers = [];
+    for x in range(len(depicts)):
+        depicts_data = depicts[x]['mainsnak'];
+        wd_item = depicts_data['datavalue']['value'];
+        wd_item_id = wd_item['id'];
+        q_numbers.append(wd_item_id);
 
-    # Isolate the WikiData Item
-    wd_item = depicts_data['datavalue']['value']
+    if verbose:
+        print(f'Commons Page ID:{wc_page_id} -> Depicts:{q_numbers}');
 
-    # Extract the WikiData Item ID Corresponding to the original Commons File
-    wd_item_id = wd_item['id']
-
-    print(f'Commons Page ID:{wc_page_id} -> WikiData Item ID:{wd_item_id}')
-    return wd_item_id
+    return q_numbers;
 
 
 def get_wikidata_label (wd_item_id):
@@ -92,7 +92,6 @@ def get_wikidata_label (wd_item_id):
     if 'entities' in data and wd_item_id in data['entities'] and 'labels' in data['entities'][wd_item_id] and 'en' in data['entities'][wd_item_id]['labels']:
         label = data['entities'][wd_item_id]['labels']['en']['value'];
     else:
-        # label = 'label not found';
         print(f'No depicts label found for {wd_item_id}');
         return None;
 
@@ -108,47 +107,58 @@ def get_wikidata_label (wd_item_id):
 #       df.to_csv('df_wiki.csv');
 #
 #
-# >>> df = create_df(10)
-#   wiki_commons_id wiki_data_id                         depicts
-# 0       136824891       Q34442                            road
-# 1       140400994       Q11451                     agriculture
-# 2       146977661         Q525                             Sun
-# 3        61900027     Q1349184                     Jerry Rubin
-# 4         6308109     Q2715627        Semaprochilodus insignis
-# 5        94952265    Q61913577  town hall of Neuilly-sur-Seine
-# 6        82882839    Q69482485      Onze Afgevaardigden (1913)
-# 7       112921622    Q12292478                          estate
-# 8       141399896    Q42958932                  Quinta Capurro
-# 9       146926387     Q8195195                         Alfonso
+# >>> df = create_df(10, verbose=False, csv_name='wahoo_for_you.csv')
+#   wiki_commons_id                                      wiki_data_q                                            depicts
+# 0       132900617               [Q22698, Q188509, Q34442, Q967166]                        [park, suburb, road, Hythe]
+# 1       116975754  [Q11451, Q1006733, Q1639395, Q30121, Q16542738]  [agriculture, grassland, bridle path, pasture,...
+# 2       146501368                                           [Q525]                                              [Sun]
+# 3        20439034                               [Q836934, Q448018]                             [cheongsam, Hiro Saga]
+# 4       110956109                                      [Q20204357]                            [St. Ägidius (Grafing)]
+# 5       130031795                 [Q1081138, Q105889895, Q1894394]         [historic site, religious site, Martinhoe]
+# 6       108706552                              [Q1468524, Q179351]                 [city center, City of Westminster]
+# 7       106966797                       [Q1311670, Q12280, Q29245]            [rail infrastructure, bridge, Hastings]
+# 8        55368967                                      [Q38284967]                           [Aqueduct of Agia Irini]
+# 9        23636550                                      [Q17404929]                           [Kerkstraat 14, Leveroy]
 
-def create_df (row_count_wanted):
+def create_df (row_count_wanted, verbose=False, csv_name=''):
     '''
         Parameters:
             • row_count_wanted [int]: Specify the desired number of rows for the resulting dataframe to contain.
+            • verbose [bool]: Print the dataframe each time we add a new row as it is being built.
+            • csv_name [str]: Create a csv file named `csv_name` with the depicts dataframe.
+        Notes:
+            • It's possible that the number of elements in the `wiki_data_q` column might not equal the number of elements in the corresponding `depicts` column (?)
+        Example Call:
+            • df = create_df(10, verbose=False, csv_name='example_call')
     '''
-    df_wiki = pd.DataFrame({'wiki_commons_id':[], 'wiki_data_id':[], 'depicts':[]});
+    df_wiki = pd.DataFrame({'wiki_commons_id':[], 'wiki_data_q':[], 'depicts':[]});
 
     df_row_count = 0;
     enough_rows = False;
 
     while not enough_rows:
-        row = {'wiki_commons_id':'', 'wiki_data_id':'', 'depicts':''};
-        wc_page_ids = get_random_commons_ids(25); print(f'Commons IDs:\n{wc_page_ids}');
+        row = {'wiki_commons_id':'', 'wiki_data_q':'', 'depicts':''};
+        wc_page_ids = get_random_commons_ids(10*row_count_wanted); print(f'\nTrying Commons ID Batch:\n{wc_page_ids}');
 
         for wc_page_id in wc_page_ids:
-            wd_q_number = get_q_number(wc_page_id);
-            if wd_q_number != 0:
-                wd_depicts_statement = get_wikidata_label(wd_q_number);
+            q_numbers = get_q_numbers(wc_page_id);
+            if q_numbers:
+                wd_depicts_statements = [get_wikidata_label(x) for x in q_numbers];
 
-                if wd_depicts_statement:
-                    row['wiki_commons_id'] = wc_page_id;
-                    row['wiki_data_id'] = wd_q_number;
-                    row['depicts'] = wd_depicts_statement;
-                    print(f'Row: {row}');
+                if wd_depicts_statements:
+                    row['wiki_commons_id']  = wc_page_id;
+                    row['wiki_data_q']      = q_numbers;
+                    row['depicts']          = wd_depicts_statements;
+
+                    if verbose:
+                        print(f'Row: {row}');
 
                     df_row = pd.DataFrame.from_dict(row, orient='index').T;
 
-                    df_wiki = pd.concat([df_wiki, df_row], ignore_index=True); print(f'Updated DataFrame:\n{df_wiki}');
+                    df_wiki = pd.concat([df_wiki, df_row], ignore_index=True);
+
+                    if verbose:
+                        print(f'Updated DataFrame:\n{df_wiki}');
 
                     df_row_count = df_wiki.shape[0];
 
@@ -156,6 +166,15 @@ def create_df (row_count_wanted):
                         enough_rows = True;
                         break;
 
+    #
+    # If desired, create the csv file
+    #
+
+    if csv_name:
+        # Add the file handle if the user forgot to specify
+        if not csv_name.endswith('.csv'):
+            csv_name = csv_name + '.csv';
+        df_wiki.to_csv(csv_name, index=False);
     return df_wiki;
 
 
@@ -169,7 +188,7 @@ def create_df (row_count_wanted):
 
 # Commons Page ID:61396200 -> WikiData Item ID:Q7186
 wc_page_id = 61396200;
-wd_item_id = get_q_number(wc_page_id);
+wd_item_id = get_q_numbers(wc_page_id);
 
 
 
@@ -179,7 +198,7 @@ wd_item_id = get_q_number(wc_page_id);
 
 # Commons Page ID:71048738 -> WikiData Item ID:Q1042920
 wc_page_id = 71048738;
-wd_item_id = get_q_number(wc_page_id);
+wd_item_id = get_q_numbers(wc_page_id);
 
 
 #
@@ -188,4 +207,4 @@ wd_item_id = get_q_number(wc_page_id);
 #
 
 wc_page_id = get_random_commons_ids(num_files=1);
-wd_item_id = get_q_number(wc_page_id);
+wd_item_id = get_q_numbers(wc_page_id);
